8c2b98e32a8091974c782aaff24b4065e6c148e2
Julian Steinwachs <julian.steinwachs@fau.de>
2015-12-29 15:41:49 +0100

    decryption seams to work

--- index.js
+++ index.js
@@ -212,8 +212,10 @@ console.log("\n the following hashes should be equal")
 console.log(hmac_val)
 console.log(sec_mac)
 
+var iv = new Buffer(16);
+console.log(iv)
 
-var decrypter = Crypto.createDecipheriv("aes-256-cbc",aes_key,new Buffer(16))
+var decrypter = Crypto.createDecipheriv("aes-256-cbc",aes_key,iv)
 decrypter.setAutoPadding()
 var out = [];
 out.push(decrypter.update(sec_body))
